c87b8d2add0522128d986f59934bd3cfa38326bb
Won Maung Thein <wonmaungthein@gmail.com>
2018-11-10 12:54:21 +0000

    Added register post code

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 // Silence is golden.
+add_action('init', function () {
+    register_post_type('people', [
+        'labels' => [
+            'name' => __('People'),
+            'singular_name' => __('Person'),
+        ],
+        'public' => true,
+        'has_archive' => true,
+    ]);
+});
