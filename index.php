aacdc05ed60f2dbb3cfa3c7b333353b85d520c0b
Won Maung Thein <wonmaungthein@gmail.com>
2018-11-10 12:55:44 +0000

    Deleted unnecessary code

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
 <?php
 // Silence is golden.
-add_action('init', function () {
-    register_post_type('people', [
-        'labels' => [
-            'name' => __('People'),
-            'singular_name' => __('Person'),
-        ],
-        'public' => true,
-        'has_archive' => true,
-    ]);
-});
